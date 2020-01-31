41eccd1a555f13d8fe99aa5f174d6752e3d3d3a0
David Vélez <davidvelezg@hotmail.com>
2020-01-31 23:43:27 +0100

    Añadir Entity Framework y entidades bd

---
+++ Coworking.Api.DataAccess/CoworkingDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Coworking.Api.DataAccess.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coworking.Api.DataAccess
+{
+    public class CoworkingDBContext : DbContext, ICoworkingDBContext
+    {
+        public CoworkingDBContext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
--- Coworking.api/Startup.cs
+++ Coworking.api/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Coworking.Api.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -19,11 +21,13 @@ namespace Coworking.api
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }    
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CoworkingDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DataBaseConnection")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
