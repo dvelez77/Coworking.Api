b556776b4396fa1cc162678420aa9af0ee431562
David Vélez <davidvelezg@hotmail.com>
2020-02-01 20:41:08 +0100

    Añadir configuracion entidades

--- Coworking.Api.DataAccess.Contracts/ICoworkingDBContext.cs
+++ Coworking.Api.DataAccess.Contracts/ICoworkingDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Coworking.Api.DataAccess.Contracts.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,6 +7,14 @@ using System.Text;
 namespace Coworking.Api.DataAccess.Contracts
 {
     public interface ICoworkingDBContext
-    { 
+    {
+        DbSet<UserEntity> Users { get; set; }
+        DbSet<AdminEntity> Admins { get; set; }
+        DbSet<BookingEntity> Bookings { get; set; }
+        DbSet<OfficeEntity> Offices { get; set; }
+        DbSet<Offices2RoomsEntity> Offices2Rooms { get; set; }
+        DbSet<Room2ServicesEntity> Room2Services { get; set; }
+        DbSet<RoomEntity> Rooms { get; set; }
+        DbSet<ServiceEntity> Services { get; set; }
     }
 }
